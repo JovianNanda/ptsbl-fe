72634df8858a7cf1e0544df3073c2ed6a0db0c9a
Jovian Nanda <joviansismawan13@gmail.com>
2025-11-12 20:32:55 +0800

    feat: change CTA language

--- app/components/service/CallToAction.vue
+++ app/components/service/CallToAction.vue
@@ -7,11 +7,10 @@
       class="max-w-3xl w-full ring-0 border-0 outline-0 rounded-3xl shadow-xl p-10 bg-white"
     >
       <h2 class="text-2xl md:text-3xl font-semibold text-gray-800 mb-4">
-        Butuh Konsultasi untuk Kebutuhan Anda?
+        {{ t("serviceCTA.title") }}
       </h2>
       <p class="text-gray-500 mb-8">
-        Tim ahli kami siap membantu Anda menemukan solusi terbaik untuk
-        pengelolaan limbah B3 perusahaan Anda
+        {{ t("serviceCTA.description") }}
       </p>
 
       <div class="flex flex-col sm:flex-row justify-center gap-4">
@@ -23,7 +22,7 @@
             color="secondary"
             class="rounded-full shadow-md px-6 text-white cursor-pointer"
           >
-            Hubungi
+            {{ t("buttonContact") }}
             {{ contacts[0]?.subtitle }}</UButton
           >
         </NuxtLink>
@@ -35,7 +34,7 @@
             size="lg"
             class="rounded-full border border-primary text-primary bg-white hover:bg-primary hover:text-white px-6 cursor-pointer"
           >
-            Email Kami
+            {{ t("serviceCTA.buttonLink") }}
           </UButton></NuxtLink
         >
       </div>
@@ -43,8 +42,10 @@
   </UContainer>
 </template>
 <script setup>
+import { useI18n } from "vue-i18n";
 import { useContactStore } from "~/stores/contact";
 
+const { t } = useI18n();
 const contactStore = useContactStore();
 await contactStore.fetchContact();
 const allContact = computed(() => contactStore?.data);
