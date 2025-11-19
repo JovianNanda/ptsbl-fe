f0b64a1bbf109c70ad12fdbf2914a8746b2e9cab
Jovian Nanda <joviansismawan13@gmail.com>
2025-11-20 00:10:17 +0800

    feat: add more favicon and webmanifest

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,6 +32,15 @@ export default defineNuxtConfig({
           type: "image/x-icon",
           href: "/favicon.ico",
         },
+        {
+          rel: "apple-touch-icon",
+          href: "/apple-touch-icon.png",
+        },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/android-chrome-192x192.png' },
+        { rel: 'icon', type: 'image/png', sizes: '512x512', href: '/android-chrome-512x512.png' },
+        { rel: 'manifest', href: '/manifest.webmanifest' }
       ],
       meta: [
         {
